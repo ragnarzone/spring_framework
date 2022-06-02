32d5bdb36a995a9cc3774357831f45cb086b245f
ragnarzone <ragnarzone@gmail.com>
2022-06-02 11:17:15 +0200

    added dependency injection with Autowired and Qualifier

--- spring_app3/src/main/java/org/chernyshov/spring/JazzMusic.java
+++ spring_app3/src/main/java/org/chernyshov/spring/JazzMusic.java
@@ -2,7 +2,7 @@ package org.chernyshov.spring;
 
 import org.springframework.stereotype.Component;
 
-
+@Component
 public class JazzMusic implements Music{
     @Override
     public String getSong() {
--- spring_app3/src/main/java/org/chernyshov/spring/MusicPlayer.java
+++ spring_app3/src/main/java/org/chernyshov/spring/MusicPlayer.java
@@ -1,6 +1,7 @@
 package org.chernyshov.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -8,19 +9,16 @@ import java.util.List;
 @Component
 public class MusicPlayer {
 
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,@Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
-
     public String playMusic(){
-//        System.out.println("Playing: " + classicalMusic.getSong());
-//        System.out.println("Playing: " + rockMusic.getSong());
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music1.getSong() + " and " + music2.getSong();
     }
 }
